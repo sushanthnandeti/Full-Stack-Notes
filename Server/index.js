893584f42a5d9ca7978f4563e8fb73ef9b13db01
Sushanth N <sushanthnandeti96@gmail.com>
2023-11-24 01:39:40 -0500

    Client and Server Successful connection

---
+++ Server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const db = require("./models");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+//Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+
+db.sequelize.sync().then(() => {
+
+    app.listen(3001, () => {
+        console.log("Server running on Port 3001");
+        
+    });
+
+});
---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+
+function App() {
+
+  const [listOfPorts, setListOfPosts] = useState([]);
+
+  useEffect(()=>{
+    axios.get('http://localhost:3001/posts').then((response) => {
+      setListOfPosts(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      {listOfPorts.map((value, key)=>{
+    
+      return (
+              <div className="post">
+                  <div className="title">   {value.title}  </div>
+                  <div className="body">   {value.postText}  </div>
+                  <div className="footer">   {value.username}  </div>
+              </div> 
+      );
+    })}
+    </div>
+  );
+}
+
+export default App;
